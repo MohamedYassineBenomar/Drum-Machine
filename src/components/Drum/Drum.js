9d4800864f1f3dde5744355c70ee146c6497bef0
MohamedYassineBenomar <mohamedyassiine@hotmail.com>
2023-10-11 23:38:11 +0100

    add key event listeners

--- src/components/Drum/Drum.js
+++ src/components/Drum/Drum.js
@@ -4,38 +4,93 @@ import SoundSwitch from '../PowerSwitch/PowerSwitch';
 import BankSwitch from '../BankSwitch/BankSwitch';
 import LogsScreen from '../LogsScreen/LogsScreen';
 import Volumebar from '../Volumebar/Volumebar'
+import { useEffect } from 'react';
 
 function Drum() {
+  useEffect(() => {
+    document.addEventListener('keydown', function(event) {
+      var btn;
+      switch (event.key) {
+        case 'a':
+          btn = document.getElementById('a_note');
+          break;
+        case 'z':
+          btn = document.getElementById('z_note');
+          break;
+        case 'e':
+          btn = document.getElementById('e_note');
+          break;
+        case 'q':
+          btn = document.getElementById('q_note');
+          break;
+        case 's':
+          btn = document.getElementById('s_note');
+          break;
+        case 'd':
+          btn = document.getElementById('d_note');
+          break;
+        case 'w':
+          btn = document.getElementById('w_note');
+          break;
+        case 'x':
+          btn = document.getElementById('x_note');
+          break;
+        case 'c':
+          btn = document.getElementById('c_note');
+          break;
+        default:
+          return; // Ignore keys other than a, z, e, q, s, d, w, x, c
+      }
+  
+      if (btn) {
+        btn.classList.add('btn-active'); // Add a CSS class that triggers the button's active state
+        setTimeout(function() {
+          btn.classList.remove('btn-active'); // Remove the CSS class after a short delay
+        }, 100);
+        btn.click();
+      }
+    });
+  }, []);
+  
+
+ 
+
+  /*const addeventlisteners = () => {
+
+  }*/
+  const handleplaysound = (event) => {
+    console.log(event.target.innerText)
+  }
   return (
     <div className='container'>
       <div className='drum'>
         <div className='rings-panel'>
           
-          <button className='btns-rings'>
+          <button id='a_note' className='btns-rings' onClick={handleplaysound}>
             a
           </button>
-          <button className='btns-rings'>
+          <button id='z_note' className='btns-rings'>
             z
           </button>
-          <button className='btns-rings'>
+          <button id='e_note' className='btns-rings'>
             e
           </button>
-          <button className='btns-rings'>
+          <button id='q_note' className='btns-rings'>
             q
           </button>
-          <button className='btns-rings'>
+          <button id='s_note' className='btns-rings'>
             s
           </button>
-          <button className='btns-rings'>
+          <button id='d_note' className='btns-rings'>
             d
           </button>
-          <button className='btns-rings'>
+          <button id='w_note' className='btns-rings'>
             w
           </button>
-          <button className='btns-rings'>
+          <button id='x_note' className='btns-rings'>
             x
           </button>
-          <button className='btns-rings'>
+          <button id='c_note' className='btns-rings'>
             c
           </button>
         </div>
